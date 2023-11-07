aae41780ecbea1cfd1bbf1adb283a591b4451f88
ItaloCobains <italo.brandao@simpleagro.com.br>
2023-11-07 11:12:21 -0300

    paging

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,15 @@ use core::panic::PanicInfo;
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
 
 pub fn init() {
     gdt::init();
@@ -72,8 +79,8 @@ pub fn hlt_loop() -> ! {
 
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    // like before
     init();
     test_main();
     hlt_loop();
